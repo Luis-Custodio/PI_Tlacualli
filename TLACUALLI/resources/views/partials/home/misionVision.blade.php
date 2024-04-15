0ba3f201e2eefc35a5f6acdbf00825c263770e11
LuisG-Custodio <121039302@upq.edu.mx>
2024-04-15 15:47:01 -0600

    Cambios primarios

--- TLACUALLI/resources/views/partials/home/misionVision.blade.php
+++ TLACUALLI/resources/views/partials/home/misionVision.blade.php
@@ -4,21 +4,21 @@
             <div class="feature-box h-100 text-center">
                 <div class="feature-box-icon"><img class="w-25" src="https://outreach.engineering.columbia.edu/sites/default/files/styles/cu_crop/public/content/9_0.jpg?itok=BRQtZyOJ" alt=""></div>
                 <h3 class="feature-box-title">¿Cómo ayudamos?</h3>
-                <p class="feature-box-desc">Depending in newspaper an September do existence strangers. Total great saw water had mirth happy new relation diverted in.</p>
+                <p class="feature-box-desc">En Tlacualli, ofrecemos información, recursos y herramientas prácticas para que las personas y comunidades puedan comenzar y mantener sus propios sistemas de compostaje. Además, brindamos asesoramiento personalizado y soluciones adaptadas a las necesidades específicas de cada cliente, desde hogares hasta empresas y organizaciones, para que puedan adoptar prácticas de compostaje de manera efectiva y sostenible.</p>
             </div>
         </div>
         <div class="col-md-4">
             <div class="feature-box h-100 text-center">
                 <div class="feature-box-icon"><img class="w-25" src="https://th.bing.com/th/id/R.52c427be375383133a8230d8f87ae826?rik=jAkykku6wc7cYg&riu=http%3a%2f%2fecofriendlytower.com%2fwp-content%2fuploads%2f2018%2f11%2fmvv1.png&ehk=qpb0lmRvr9Yl3M7TzMOSGW6eZVvKVFY5qVIud8ovz8A%3d&risl=&pid=ImgRaw&r=0" alt=""></div>
                 <h3 class="feature-box-title mt-4">Misión</h3>
-                <p class="feature-box-desc">Continue delicate as unlocked entirely mr relation diverted in. Known not end fully being style house totally of in chiefly.</p>
+                <p class="feature-box-desc">Nuestra misión es promover prácticas sostenibles de compostaje para reducir los residuos orgánicos y fomentar la fertilidad del suelo, contribuyendo así a un medio ambiente más saludable y a la construcción de comunidades más conscientes y responsables.</p>
             </div>
         </div>
         <div class="col-md-4">
             <div class="feature-box h-100 text-center">
                 <div class="feature-box-icon"><img class="w-25" src="https://pacindia.org/storage/images/vision%20green.png" alt=""></div>
                 <h3 class="feature-box-title">Visión</h3>
-                <p class="feature-box-desc">Outlived it received he material. Took yourself joy moderate off repeated laughter outweigh screened spot soon led size you. </p>
+                <p class="feature-box-desc">Nuestra visión es ser líderes en la promoción del compostaje a nivel local y global, creando un mundo donde el compostaje sea una práctica común y valorada, ayudando a mitigar el impacto de los residuos orgánicos en el medio ambiente y a promover la regeneración de ecosistemas.</p>
             </div>
         </div>
     </div>
